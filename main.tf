657077548f213ceddeed017ea846570fe7b7c033
Premnayak55 <33243781+Premnayak55@users.noreply.github.com>
2021-05-18 12:17:12 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.15.3"
-}
-
 provider "aws" {
   profile = "default"
-  region  = var.aws_region
-  access_key = "AKIAROP3SMDZYQX3G7FA"
-  secret_key = "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"
+  region  = "us-east-1"
+  access_key = "AKIAROP3SMDZ63BRS5PG"
+  secret_key = "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"
 }
-
-resource "aws_instance" "app_server" {
+resource "aws_instance" "useast1" {
   ami           = "ami-0d5eff06f840b45e9"
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "Jenkins instance_type"
-  }
 }
